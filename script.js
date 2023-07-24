17b5a6c067cada14c125145a4fa49ff94307edda
Utsav Singh <utsavgwa@gmail.com>
2023-07-24 15:49:44 +0530

    added html, css files

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelector("html").addEventListener("click", openFullscreen);
+
+var elem = document.querySelector(".video-background");
+
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) {
+    /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
